9da4f287de20674d5ba24c3bc807c3835d48da0c
FORAM PATEL <foram.patel@stonybrook.edu>
2023-02-12 03:12:50 -0500

    format

--- src/Stdy.js
+++ src/Stdy.js
@@ -1,10 +1,25 @@
-
 import "./css/Stdy.css";
-
 
 let hasStudyPageBeenCreated = false;
 
 function createStudyPage() {
+
+    const buttonContainer = document.createElement("div");
+buttonContainer.style.position = "fixed";
+buttonContainer.style.top = "0";
+buttonContainer.style.left = "0";
+
+const homeButton = document.createElement("button");
+homeButton.textContent = "Home";
+homeButton.style.backgroundColor = "white";
+homeButton.style.color = "black";
+homeButton.style.fontSize = "24px";
+homeButton.style.padding = "12px";
+homeButton.style.border = "1px solid black";
+
+buttonContainer.appendChild(homeButton);
+document.body.appendChild(buttonContainer);
+
   if (!hasStudyPageBeenCreated) {
     const buttonContainer = document.createElement("div");
     buttonContainer.style.display = "flex";
@@ -38,6 +53,7 @@ function createStudyPage() {
       button.style.borderRadius = "4px";
       button.style.width = "200px";
       button.style.height = "200px";
+      button.style.border = "10px solid white";
 
       if (index % 2 === 0) {
         const row = document.createElement("div");
@@ -57,7 +73,3 @@ function createStudyPage() {
 }
 
 export default createStudyPage;
-
-
-
-

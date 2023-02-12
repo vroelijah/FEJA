47f087c30add77ee8b3b18b456824f268a322cfb
FORAM PATEL <foram.patel@stonybrook.edu>
2023-02-12 02:00:19 -0500

    stdy

---
+++ src/Stdy.js
@@ -0,0 +1,29 @@
+
+import "./css/Stdy.css";
+
+let Stdy=()=>{
+    return(
+    <body>
+        <h1>
+            Study Spots
+        </h1>
+        <h2>
+            Melville Library
+        </h2>
+        <h2>
+            SAC
+        </h2>
+        <h2>
+            Union
+        </h2>
+    </body>
+
+
+
+
+
+
+    )
+}
+
+export default Stdy ;
